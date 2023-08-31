5045cd50ed6ba8f820878ac7f6f0bb120a3fa2bd
Jhosh-Christopher <shuanbrj@gmail.com>
2023-08-31 00:15:58 -0300

    Criação das validações dentro do método

--- src/models/propriedades.js
+++ src/models/propriedades.js
@@ -1,9 +1,23 @@
 class Propriedades {
-    propriedades = []
 
-    addPropiedades(propriedade){
-        this.propriedades.push(propriedade)
+    constructor(valorInicial) {
+        this.propriedades = []
+
+        if (valorInicial) {
+            this.addPropriedade(valorInicial)
+        }
+    }
+
+    addPropriedade(propriedade){
+        if (isNaN(propriedade)) { 
+            this.propriedades.push(propriedade.toLowerCase())
+        }
+    }
+
+    getPropriedadesOrdenadas() {
+        return this.propriedades.filter(propriedade => propriedade !== null).sort()
     }
 }
+
 
 module.exports = {Propriedades}
